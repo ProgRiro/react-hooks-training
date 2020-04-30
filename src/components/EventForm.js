2cb4048392dc2e4138bb5b72d3ea6011934a21bb
progriro <progriro@gmail.com>
2020-04-30 21:08:04 +0900

    create operationLogs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,12 @@
 import React, { useContext, useState } from 'react';
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions/index';
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS,
+} from '../actions/index';
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -16,6 +22,12 @@ const EventForm = () => {
       body,
     });
 
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601(),
+    });
+
     setTitle('');
     setBody('');
   };
@@ -25,7 +37,14 @@ const EventForm = () => {
     const result = window.confirm(
       '全てのイベントを本当に削除してもいいですか？'
     );
-    if (result) dispatch({ type: DELETE_ALL_EVENTS });
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENTS });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601(),
+      });
+    }
   };
 
   const unCreatable = title === '' || body === '';
